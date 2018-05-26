3bb4146e2593774d5ec513e62aeb2611ed13b57e
yonic <yon@yon>
2018-05-26 22:16:45 +0300

    Add deep full examp,e

--- models.py
+++ models.py
@@ -44,6 +44,23 @@ class InceptionModel(object):
         probs = output.op.inputs[0]
         return probs
     
+class InceptionModelLogits(object):
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+        self.built = False
+
+    def __call__(self, x_input):
+        """Constructs model and return probabilities for given input."""
+        reuse = True if self.built else None
+        with slim.arg_scope(inception.inception_v3_arg_scope()):
+            _, end_points = inception.inception_v3(
+                            x_input, num_classes=self.num_classes, is_training=False,
+                            reuse=reuse)
+        self.built = True
+        output = end_points['Logits']
+        logits = output.op.inputs[0]
+        return logits
+    
     
 '''Create adversarial images from the orignal data set
 '''
@@ -56,15 +73,18 @@ def adversarial_generator(mode,batch_shape,eps,is_return_orig_images=False):
         print("{} generator graph is ready!".format(mode))
         with graph_fgsm.as_default():    
             x_input = tf.placeholder(tf.float32, shape=batch_shape)
-            model = InceptionModel(importer.num_classes)        
+                   
             
             params = {'eps':eps}
             if mode == 'fgsm':
+                model = InceptionModel(importer.num_classes) 
                 graph = FastGradientMethod(model)
             elif mode == 'ifgsm':
+                model = InceptionModel(importer.num_classes) 
                 params['nb_iter'] = 10
                 graph  = BasicIterativeMethod(model)                
             elif mode == 'deep_fool':
+                model = InceptionModelLogits(importer.num_classes) 
                 graph = DeepFool(model)
                 params['max_iter'] = 5
             elif mode == 'carlini_wagner':
@@ -112,7 +132,7 @@ def test_deep_full_simple_model():
     sess = tf.Session()
     model = SimpleModel()
     attack = DeepFool(model=model, sess=sess)
-    x_val = np.random.rand(100, 2)
+    x_val = np.random.rand(1, 2)
     x_val = np.array(x_val, dtype=np.float32)
 
     x_adv = attack.generate_np(
@@ -123,12 +143,13 @@ def test_deep_full_simple_model():
     print("adversarial_images shape:{}".format(adversarial_images.shape))
     
     
+    
 def test_deep_full_inception_v3_model():
     class Inception_V3_Model(Model):
         """A very simple neural network
         """        
         def get_logits(self, x_input):
-            NUM_CLASSES = 2
+            NUM_CLASSES = 1000
             """Constructs model and return probabilities for given input."""
             #reuse = True if self.built else None
             with slim.arg_scope(inception.inception_v3_arg_scope()):
@@ -136,13 +157,45 @@ def test_deep_full_inception_v3_model():
                                 x_input, num_classes=NUM_CLASSES, is_training=False,
                                 reuse=False)
             self.built = True
-            output = end_points['Predictions']
+            output = end_points['Logits']
+            probs = output.op.inputs[0]
+            return probs
+    
+    
+    sess = tf.Session()
+    model = Inception_V3_Model()
+    attack = DeepFool(model=model, sess=sess)
+    x_val = np.random.rand(1, 299,299,3)
+    x_val = np.array(x_val, dtype=np.float32)
+
+    x_adv = attack.generate_np(
+            x_val, over_shoot=0.02, max_iter=5,
+            nb_candidate=2, clip_min=-5,
+            clip_max=5)    
+    adversarial_images = sess.run(model(x_adv))    
+    print("adversarial_images shape:{}".format(adversarial_images.shape))
+    
+    
+def test_deep_full_inception_v3_model__():
+    class Inception_V3_Model(Model):
+        """A very simple neural network
+        """        
+        def get_logits(self, x_input):
+            NUM_CLASSES = 1000
+            """Constructs model and return probabilities for given input."""
+            #reuse = True if self.built else None
+            with slim.arg_scope(inception.inception_v3_arg_scope()):
+                _, end_points = inception.inception_v3(
+                                x_input, num_classes=NUM_CLASSES, is_training=False,
+                                reuse=False)
+            self.built = True
+            output = end_points['Logits']
             probs = output.op.inputs[0]
             return probs
         
         
     
-    x_val = np.random.rand(100, 299,299,3)
+    x_val = np.random.rand(1, 299,299,3)
     x_val = np.array(x_val, dtype=np.float32)
     model = Inception_V3_Model()
     x_input = tf.placeholder(tf.float32, shape=[1,299,299,3])
@@ -165,6 +218,7 @@ def test_deep_full_inception_v3_model():
 
 if __name__ == "__main__":
     test_deep_full_inception_v3_model()
+    #test_deep_full_simple_model()
    
     
     
